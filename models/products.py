669d5edbe14792296536ef612437b11ee3e0b563
elianhardyy <elianhardi805@gmail.com>
2024-09-18 21:07:32 +0700

    add: products data

--- models/products.py
+++ models/products.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel
+from config.database import Base
+from sqlalchemy.schema import Column
+from sqlalchemy.types import String, Integer, DateTime
 
-class Products(BaseModel):
-    name:str
-    description:str
-    price:int
-    qty:int
+class Products(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer,primary_key=True,increment=True)
+    product_name=Column(String(255))
+    description=Column(String(255))
+    price=Column(Integer)
+    created_at=Column(DateTime)
+    updated_at=Column(DateTime)
--- routers/products/route.py
+++ routers/products/route.py
@@ -1,11 +1,19 @@
-# from fastapi import APIRouter
-# from models.products import Products
-# from config.database import collection_name
-# from schema.products import list_serial
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.orm import Session
+from models.products import Products
+from config.database import collection_name, SessionLocal
+from schema.products import list_serial
+from models.products import Products
 # from bson import ObjectId
 
-# product_route = APIRouter()
+product_route = APIRouter()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 # #GET REQUEST METHOD
 # @product_route.get("/product")
 # async def get_products():
@@ -15,3 +23,11 @@
 # @product_route.post("/post")
 # async def post_producst(products:Products):
 #     collection_name.insert_one(dict(products))
+
+@product_route.post("/add/product",status_code=status.HTTP_201_CREATED)
+async def create_product(products:Products, db:Session=Depends(get_db)):
+    db_products = Products(**products.dict())
+    db.add(db_products)
+    db.commit()
+    db.refresh(db_products)
+    return db_products
